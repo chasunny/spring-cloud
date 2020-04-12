fbfdd275a726c49f84b7331b2b3585f5c7140a04
chasunny <49181767+chasunny@users.noreply.github.com>
2020-04-12 16:00:13 +0530

    Update SpringCloudConfigServer2ApplicationTests.java

--- spring-cloud-config-server2/src/test/java/com/example/springcloudconfigserver2/SpringCloudConfigServer2ApplicationTests.java
+++ spring-cloud-config-server2/src/test/java/com/example/springcloudconfigserver2/SpringCloudConfigServer2ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.springcloudconfigserver2;
+//package com.example.springcloudconfigserver2;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringCloudConfigServer2ApplicationTests {
+//@SpringBootTest
+//class SpringCloudConfigServer2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
